fee7327b89c637d1cdd7cb0221ce61447f2db318
Javier González <javier@javigon.com>
2015-11-10 16:16:01 +0100

    add me to liblightnvm.h

--- src/liblightnvm.h
+++ src/liblightnvm.h
@@ -14,7 +14,9 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
 
- * Written by Matias Bjorling <m@bjorling.me>
+ * Written by:
+ *	- Javier Gonzalez <javier@cnexlabs.com>
+ *	- Matias Bjorling <m@bjorling.me>
  *
  * liblightnvm Linux Open-Channel I/O interface
  */
